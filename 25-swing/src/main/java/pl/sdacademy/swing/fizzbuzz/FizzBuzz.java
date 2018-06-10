e8e094533deceeefe565347257b7292145126fd2
Robert Kachniarz <kachniarz.robert@gmail.com>
2018-06-10 22:43:01 +0200

    FizzBuzz class

--- 25-swing/src/main/java/pl/sdacademy/swing/fizzbuzz/FizzBuzz.java
+++ 25-swing/src/main/java/pl/sdacademy/swing/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,14 @@
 package pl.sdacademy.swing.fizzbuzz;
 
 public class FizzBuzz {
+	public static String msg(int number){
+	String odp;
+
+		if (number % 15 == 0) odp = "FizzBuzz";
+		else if (number % 5 == 0) odp = "Buzz";
+		else if (number % 3 == 0) odp = "Fizz";
+		else odp = Integer.toString(number);
+
+		return odp;
+	}
 }
