b1d052887cab2cbf47514259d6098abb0ffc43de
Robert Kachniarz <kachniarz.robert@gmail.com>
2018-06-16 00:43:53 +0200

    FizzBuzzWhiz for number 1

---
+++ 26-tdd/src/main/java/pl/sdacademy/tdd/FizzBuzzWhiz.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.tdd;
+
+public class FizzBuzzWhiz {
+	public static String fizzBuzzWhiz (int number){
+		String s="";
+		if (number == 1) s = "1";
+		return s;
+	}
+}
---
+++ 26-tdd/src/test/java/FizzBuzzWhizTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import pl.sdacademy.tdd.FizzBuzzWhiz;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FizzBuzzWhizTest {
+	// @DisplayName("jakiś opis");
+	/*
+	Same as FizzBuzz but substitute prime numbers with "Whiz"
+	Write some code that will generate a string of integers, starting at 1 and going up to 100, all separated
+	by commas. Substitute any integer which is divisible by 3 with "Fizz", and any integer which is divisible
+	by 5 with "Buzz", and any integer divisible by 3 and 5 with "FizzBuzz".
+	*/
+	 
+	@Test
+	void fizBuzzAndPrime() {
+
+		//given
+		int n = 0;
+
+		//when
+		int number = 0;
+
+		//then
+		assertThat(FizzBuzzWhiz.fizzBuzzWhiz(1)).isEqualTo("1");
+
+
+
+	}
+}
+
