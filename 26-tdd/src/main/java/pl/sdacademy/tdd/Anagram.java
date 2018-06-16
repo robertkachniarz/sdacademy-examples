f156a84c07103febb374291782b72076639f5a6d
Robert Kachniarz <kachniarz.robert@gmail.com>
2018-06-16 13:39:39 +0200

    Add Anagram project and hr-module

--- 26-tdd/src/main/java/pl/sdacademy/tdd/Anagram.java
+++ 26-tdd/src/main/java/pl/sdacademy/tdd/Anagram.java
@@ -1,4 +1,33 @@
 package pl.sdacademy.tdd;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Anagram {
+	public static Set<String> converter(String str) {
+		char strCharList[] = str.toCharArray();
+		Set<String> anagram = new HashSet<>();
+		String strLocalValue = "";
+			for (int i = 0; i < strCharList.length; i++){
+				strLocalValue+=strCharList[i];
+			for (int j = 0; j < strCharList.length; j++){
+				if (i!=j) {
+					strLocalValue += strCharList[j];
+				}
+			}
+			anagram.add(strLocalValue);
+			strLocalValue = "";
+
+				strLocalValue+=strCharList[i];
+				for (int j = strCharList.length - 1; j >= 0; j--){
+					if (i!=j) {
+						strLocalValue += strCharList[j];
+					}
+				}
+				anagram.add(strLocalValue);
+				strLocalValue = "";
+			}
+
+		return anagram;
+	}
 }
--- 26-tdd/src/test/java/AnagramTest.java
+++ 26-tdd/src/test/java/AnagramTest.java
@@ -1,2 +1,68 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import pl.sdacademy.tdd.Anagram;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class AnagramTest {
+	@Disabled
+	@Test
+	void siep(){
+		//given
+		String str = "pies";
+		//when
+		Set<String> piescidelko = Anagram.converter(str);
+		//then
+		assertThat(piescidelko).contains("siep");
+	}
+	@Disabled
+	@Test
+	void epis(){
+		//given
+		String str = "pies";
+		//when
+		Set<String> piescidelko = Anagram.converter(str);
+		//then
+		assertThat(piescidelko).contains("epis");
+	}
+	@Test
+	void allJo(){
+		//given
+		String str = "jo";
+		//when
+		Set<String> piescidelko = Anagram.converter(str);
+		//then
+		assertThat(piescidelko).containsOnly("oj");
+	}
+	@Test
+	void allOo(){
+		//given
+		String str = "jo";
+		//when
+		Set<String> piescidelko = Anagram.converter(str);
+		//then
+		assertThat(piescidelko).containsOnly("oo");
+	}
+	@Test
+	void allKot(){
+		//given
+		String str = "kot";
+		//when
+		Set<String> piescidelko = Anagram.converter(str);
+		//then
+		assertThat(piescidelko).containsOnly("kot", "kto", "tko", "tok", "okt", "otk");
+	}
+
+	@Test
+	void allPies(){
+		//given
+		String str = "pies";
+		//when
+		Set<String> piescidelko = Anagram.converter(str);
+		//then
+		assertThat(piescidelko).containsOnly("pies", "pise", "peis", "pesi", "psie", "psei");
+	}
+
 }
