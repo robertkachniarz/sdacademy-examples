262f18d6d72fe241f27e9be708c5a7762bbea626
Robert Kachniarz <kachniarz.robert@gmail.com>
2018-06-23 23:32:51 +0200

    Add SortingAlgorithms with BubbleSort: Test2 for array with two numbers

--- 26-tdd/src/main/java/pl/sdacademy/tdd/SortingAlgorithms.java
+++ 26-tdd/src/main/java/pl/sdacademy/tdd/SortingAlgorithms.java
@@ -2,6 +2,11 @@ package pl.sdacademy.tdd;
 
 public class SortingAlgorithms {
 	public static int[] bubbleSort(int[] arr){
+		if (arr[0] > arr[1]){
+			int temp = arr[0];
+			arr[0] = arr[1];
+			arr[1] = temp;
+		}
 		return arr;
 	}
 
--- 26-tdd/src/test/java/SortingAlgorithmsTest.java
+++ 26-tdd/src/test/java/SortingAlgorithmsTest.java
@@ -16,4 +16,15 @@ public class SortingAlgorithmsTest {
 		//then
 		assertThat(arr).containsExactly(resultArray);
 	}
+
+	@DisplayName("Should return sorted array with two numbers when given is unsorted array with two numbers")
+	@Test
+	void bubbleSortTest2(){
+		//given
+		int[] arr = {7, 5};
+		//when
+		int[] resultArray = SortingAlgorithms.bubbleSort(arr);
+		//then
+		assertThat(resultArray).isSorted();
+	}
 }
