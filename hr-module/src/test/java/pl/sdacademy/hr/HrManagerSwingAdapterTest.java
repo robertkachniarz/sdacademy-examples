17acdd2db5adec4ed1052d630fa8b6180a29898c
Robert Kachniarz <kachniarz.robert@gmail.com>
2018-06-30 12:53:47 +0200

    Add tests to sort methods in swing adapter

--- hr-module/src/test/java/pl/sdacademy/hr/HrManagerSwingAdapterTest.java
+++ hr-module/src/test/java/pl/sdacademy/hr/HrManagerSwingAdapterTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.table.DefaultTableModel;
+import java.util.Arrays;
+import java.util.List;
 
 public class HrManagerSwingAdapterTest {
 
@@ -44,4 +46,85 @@ public class HrManagerSwingAdapterTest {
 		};
 	}
 
+	@DisplayName("should add two rows to empty table model matching sorted HrManager data ")
+	@Test
+	void test02(){
+		//given
+		// create empty table model
+		DefaultTableModel tableModel = new DefaultTableModel();
+		tableModel.addColumn("First Name");
+		tableModel.addColumn("Last Name");
+		tableModel.addColumn("Date of Birth");
+		HrManagerSwingAdapter adapter = new HrManagerSwingAdapter(fakeSortHrManager());
+		//when
+		adapter.sortByFirstName(tableModel);
+		//then
+		//row 0
+		Object firstNameFromTableModel = tableModel.getValueAt(0, 0);
+		Assertions.assertThat(firstNameFromTableModel).isEqualTo("Tomek");
+		Object lastNameFromTableModel = tableModel.getValueAt(0, 1);
+		Assertions.assertThat(lastNameFromTableModel).isEqualTo("Tomczuk");
+		Object dateOfBirth = tableModel.getValueAt(0, 2);
+		Assertions.assertThat(dateOfBirth).isEqualTo("09-09-1999");
+		// row1
+		Object firstNameFromTableMode2 = tableModel.getValueAt(1, 0);
+		Assertions.assertThat(firstNameFromTableMode2).isEqualTo("Igor");
+		Object lastNameFromTableMode2 = tableModel.getValueAt(1, 1);
+		Assertions.assertThat(lastNameFromTableMode2).isEqualTo("Nowak");
+		Object dateOfBirth2 = tableModel.getValueAt(1, 2);
+		Assertions.assertThat(dateOfBirth2).isEqualTo("08-08-1899");
+	}
+
+	private HrManager fakeSortHrManager() {
+		return new HrManager(){
+			@Override
+			public List<Employee> sortByFirstName() {
+				return Arrays.asList(new Employee("Tomek", "Tomczuk", "09-09-1999"),
+					new Employee("Igor", "Nowak", "08-08-1899"));
+
+			}
+		};
+	}
+
+	@DisplayName("Should replace all existing rows ")
+	@Test
+	void test03 (){
+		//given
+		//create empty table model
+		DefaultTableModel tableModel = new DefaultTableModel();
+		tableModel.addColumn("First Name");
+		tableModel.addColumn("Last Name");
+		tableModel.addColumn("Date of Birth");
+		tableModel.addRow(new Object[]{"George", "Martin", "11-10-2010"});
+		HrManagerSwingAdapter adapter = new HrManagerSwingAdapter(fakeSortHrManager2());
+		//when
+		adapter.sortByLastName(tableModel);
+		//then
+		//row 0
+		Object firstNameFromTableModel = tableModel.getValueAt(0, 0);
+		Assertions.assertThat(firstNameFromTableModel).isEqualTo("Tomek");
+		Object lastNameFromTableModel = tableModel.getValueAt(0, 1);
+		Assertions.assertThat(lastNameFromTableModel).isEqualTo("Tomczuk");
+		Object dateOfBirth = tableModel.getValueAt(0, 2);
+		Assertions.assertThat(dateOfBirth).isEqualTo("09-09-1999");
+		// row1
+		Object firstNameFromTableMode2 = tableModel.getValueAt(1, 0);
+		Assertions.assertThat(firstNameFromTableMode2).isEqualTo("Igor");
+		Object lastNameFromTableMode2 = tableModel.getValueAt(1, 1);
+		Assertions.assertThat(lastNameFromTableMode2).isEqualTo("Nowak");
+		Object dateOfBirth2 = tableModel.getValueAt(1, 2);
+		Assertions.assertThat(dateOfBirth2).isEqualTo("08-08-1899");
+
+	}
+
+	private HrManager fakeSortHrManager2() {
+		return new HrManager(){
+			@Override
+			public List<Employee> sortByLastName() {
+				return Arrays.asList(new Employee("Tomek", "Tomczuk", "09-09-1999"),
+					new Employee("Igor", "Nowak", "08-08-1899"));
+
+			}
+		};
+	}
 }
